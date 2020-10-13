9b038abc3516f379715d2a4b299cacdf487eecfc
MarianoCol <marianocolman98@gmail.com>
2020-10-12 21:26:00 -0300

    arrego partida

--- partida.py
+++ partida.py
@@ -1,11 +1,11 @@
 class Partida():
 
-    def __init__(self, palabra, tipo_palabra, intentos, nombre_jugador):
-        self._palabra = list(palabra.upper())
-        self._tipo_palabra = tipo_palabra.upper()
-        self._intentos = intentos
-        self._nombre_jugador = nombre_jugador.upper()
-        self._palabra_aciertos = list()
+    def __init__(self, palabra='', tipo_palabra='', intentos=0, nombre_jugador='', palabra_aciertos=[None]):
+        self.palabra = palabra
+        self.tipo_palabra = tipo_palabra.upper()
+        self.intentos = intentos
+        self.nombre_jugador = nombre_jugador.upper()
+        self.palabra_aciertos = palabra_aciertos
 
     @property
     def palabra(self):
@@ -13,7 +13,7 @@ class Partida():
 
     @palabra.setter
     def palabra(self, palabra):
-        self._palabra = palabra
+        self._palabra = list(palabra.upper())
 
     @property
     def tipo_palabra(self):
@@ -39,13 +39,10 @@ class Partida():
     def nombre_jugador(self, nombre_jugador):
         self._nombre_jugador = nombre_jugador
 
-"""
     @property
     def palabra_aciertos(self):
         return self._palabra_aciertos
 
     @palabra_aciertos.setter
-    def palabra_aciertos(self, palabra, palabra_aciertos):
-        for i in range(len(palabra)):
-            self._palabra_aciertos = palabra_aciertos.append(None)
-"""
+    def palabra_aciertos(self, palabra_aciertos):
+        self._palabra_aciertos = palabra_aciertos * len(self._palabra)
